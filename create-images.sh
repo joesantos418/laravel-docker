562fbd61b771f15ddbc52ed3323f8cb3f232b1bd
joesantosarquivei <jose.santos@arquivei.com.br>
2021-07-16 17:55:25 -0300

    Updates librdkafka to 1.7.0

--- create-images.sh
+++ create-images.sh
@@ -2,7 +2,7 @@
 
 for PHP_VERSION in "7.3" "7.4" "8.0"
 do
-    for LIBRDKAFKA_VERSION in "v1.6.1"
+    for LIBRDKAFKA_VERSION in "v1.7.0"
     do
         for EXT_RDKAFKA_VERSION in "3.0.5" "3.1.2" "4.0.4" "4.1.2" "5.0.0"
         do
