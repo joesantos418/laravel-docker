6d24f53de0c24f2f481b4098713cf10a7c0101ca
joesantosarquivei <jose.santos@arquivei.com.br>
2021-12-09 18:13:11 -0300

    Adds support to PHP 8

--- create-images-8.sh
+++ create-images-8.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-for PHP_VERSION in "7.4"
+for PHP_VERSION in "8.0" "8.1"
 do
     for LIBRDKAFKA_VERSION in "v1.8.2"
     do
-        for EXT_RDKAFKA_VERSION in "3.0.5" "3.1.2" "4.0.4" "4.1.2" "5.0.2"
+        for EXT_RDKAFKA_VERSION in "5.0.2"
         do
             for LARAVEL_VERSION in "6" "7" "8"
             do
